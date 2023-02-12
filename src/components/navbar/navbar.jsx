9877b9205f7d1caa08c11ddb3c91bfee5f943789
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-12 23:54:53 +0200

    feat(navbar): create the navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,30 @@
+import "../../index.css";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <section className="navbar navbar-fixed-top custom-navbar" role="navigation">
+    <div className="container">
+      <div className="navbar-header">
+        <button className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span className="icon icon-bar"></span>
+          <span className="icon icon-bar"></span>
+          <span className="icon icon-bar"></span>
+        </button>
+        <a href="/" className="navbar-brand"><span className="bold">Fiduccia</span></a>
+      </div>
+      <div className="collapse navbar-collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#home" className="smoothScroll">HOME</a></li>
+          <li><a href="#about" className="smoothScroll">ABOUT</a></li>
+          <li><a href="#portfolio" className="smoothScroll">PORTFOLIO</a></li>
+          <li><a href="#team" className="smoothScroll">TEAM</a></li>
+          <li><a href="#contact" className="smoothScroll">CONTACT</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Navbar;
