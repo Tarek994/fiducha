73d289c9fac47ec8092f4a1bc4c6f9dead7ebff9
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-14 01:10:29 +0200

    feat(home.jsx): change the content of the main phrase of the home page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
           <div className="col-md-12 col-sm-12">
             <hr />
             <h3><span className="bold">WE ARE A MARKETING AGENCY</span></h3>
-            <h1 className="heading">From Egypt to the world</h1>
+            <h1 className="heading">FROM EGYPT TO THE WORLD</h1>
             <a href="#about" className="smoothScroll btn btn-default">Our Process</a>
           </div>
         </div>
