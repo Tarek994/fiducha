916ec2a285363800f4eca2b799191add594fc356
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-14 23:55:44 +0200

    test(app.js): add the services component to the app.js to test the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
+import Services from './components/services/services';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Services/>
 
       
     </div>
