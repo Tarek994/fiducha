844d0ab86eeb61e759493ce5542efd53ec91931b
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-16 23:57:53 +0200

    docs(data-projects): add the data file to mapping the project with data

---
+++ src/components/portfolio/data.jsx
@@ -0,0 +1,39 @@
+
+import portfolio_img1 from "../../images/portfolio-img1.jpg";
+import portfolio_img2 from "../../images/portfolio-img2.jpg";
+import portfolio_img3 from "../../images/portfolio-img3.jpg";
+import portfolio_img4 from "../../images/portfolio-img4.jpg";
+import portfolio_img5 from "../../images/portfolio-img5.jpg";
+import portfolio_img6 from "../../images/portfolio-img6.jpg";
+
+export const projectImages = {
+    "portfolio-img1": portfolio_img1,
+    "portfolio-img2": portfolio_img2,
+    "portfolio-img3": portfolio_img3,
+    "portfolio-img4": portfolio_img4,
+    "portfolio-img5": portfolio_img5,
+    "portfolio-img6": portfolio_img6,
+
+}
+
+export const projectDescriptions = {
+    "project-1": "ipsum dolor sit amet consectetur adipisicing",
+    "project-2": "Lorem  elit. ",
+    "project-3": "Commodi vero iure harum tempora",
+    "project-4": "quos architecto voluptatem ducimus totam odit",
+    "project-5": ", veritatis, quo consequuntur consequatur",
+    "project-6": ", veritatis, quo consequuntur consequatur",
+
+
+}
+
+export const projectNames = {
+    "Project 1": "Admin Dashboard",
+    "Project 2": "Best Portfolio",
+    "Project 3": "Social Blog",
+    "Project 4": "Apple",
+    "Project 5": "Acer website",
+    "Project 6": "Sound library",
+
+    
+  }
