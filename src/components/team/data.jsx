e02f8a7936c46713eaf770793ee1f2ca6ad6323b
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-21 23:03:11 +0200

    docs(team-data): add more of team info to mapping it

--- src/components/team/data.jsx
+++ src/components/team/data.jsx
@@ -1,16 +1,27 @@
 import team1 from "../../images/team1.jpg";
 import team2 from "../../images/team2.jpg";
 import team3 from "../../images/team3.jpg";
-import team4 from "../../images/team4.jpg";
-
-
 
 export const teamImages = {
-    "portfolio-img1": team1,
-    "portfolio-img2": team2,
-    "portfolio-img3": team3,
-    "portfolio-img4": team4,
+  "portfolio-img1": team1,
+  "portfolio-img2": team2,
+  "portfolio-img3": team3,
+};
 
+export const teamDescriptions = {
+  team1: "ipsum dolor sit amet consectetur adipisicing",
+  team2: "Lorem  elit. ",
+  team3: "Commodi vero iure harum tempora",
+  team4: "quos architecto voluptatem ducimus totam odit",
+  team5: ", veritatis, quo consequuntur consequatur",
+  team6: ", veritatis, quo consequuntur consequatur",
+};
 
-}
-
+export const teamtNames = {
+  team1: "Admin Dashboard",
+  team2: "Best Portfolio",
+  team3: "Social Blog",
+  team4: "Apple",
+  team5: "Acer website",
+  team6: "Sound library",
+};
