4cae96bfc3a911b9013571494985c903878ae3a1
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-21 22:46:15 +0200

    docs(data.jsx): add the data.jsx file to the team component

---
+++ src/components/team/data.jsx
@@ -0,0 +1,16 @@
+import team1 from "../../images/team1.jpg";
+import team2 from "../../images/team2.jpg";
+import team3 from "../../images/team3.jpg";
+import team4 from "../../images/team4.jpg";
+
+
+
+export const teamImages = {
+    "portfolio-img1": team1,
+    "portfolio-img2": team2,
+    "portfolio-img3": team3,
+    "portfolio-img4": team4,
+
+
+}
+
