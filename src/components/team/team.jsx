4c16a58c5316dfed6ab0ca25182fc90672d40ba4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-21 23:32:34 +0200

    feat(team): import the new images for the team cards

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,45 +1,57 @@
-
-
+import team1 from "../../images/team1.jpeg";
+import team2 from "../../images/team2.webp";
+import team3 from "../../images/team3.jpg";
+import "./team.css";
 
 const Team = () => {
   return (
     <section id="team">
-	<div className="container">
-		<div className="row">
-			<div className="col-md-12 col-sm-12">
-				<h1 className="heading bold">OUR TEAM</h1>
-				<h2 className="subheading">CREATIVE TALENTS FROM WEST COAST</h2>
-			</div>
-			<div className="col-md-4 col-sm-4 col-xs-6 wow fadeIn" data-wow-delay="0.9s">
-				<div className="team-wrapper">
-					<img src="images/team1.jpg" className="img-responsive" alt="team img"/>
-						<div className="team-des">
-							<h4>Tracy</h4>
-							<h3></h3>
-						</div>
-				</div>
-			</div>
-			<div className="col-md-4 col-sm-4 col-xs-6 wow fadeIn" data-wow-delay="1.3s">
-				<div className="team-wrapper">
-					<img src="images/team2.jpg" className="img-responsive" alt="team img"/>
-						<div className="team-des">
-							<h4>Linda</h4>
-							<h3></h3>
-						</div>
-				</div>
-			</div>
-			<div className="col-md-4 col-sm-4 col-xs-6 wow fadeIn" data-wow-delay="1.6s">
-				<div className="team-wrapper">
-					<img src="images/team3.jpg" className="img-responsive" alt="team img"/>
-						<div className="team-des">
-							<h4>Mary</h4>
-							<h3></h3>
-						</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 col-sm-12">
+            <h1 className="heading bold background">OUR TEAM</h1>
+            <h2 className="subheading">CREATIVE TALENTS FROM WEST COAST</h2>
+          </div>
+          <div
+            className="col-md-4 col-sm-4 col-xs-6 wow fadeIn"
+            data-wow-delay="0.9s"
+          >
+            <div className="team-wrapper">
+              <img src={team1} className="img-responsive " alt="team img" />
+              <div className="team-des">
+                <h4>Tracy</h4>
+                <h3>Social Marketing</h3>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="col-md-4 col-sm-4 col-xs-6 wow fadeIn"
+            data-wow-delay="1.3s"
+          >
+            <div className="team-wrapper">
+              <img src={team2} className="img-responsive" alt="team img" />
+              <div className="team-des">
+                <h4>Linda</h4>
+                <h3>Digital Marketing</h3>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-md-4 col-sm-4 col-xs-6 wow fadeIn"
+            data-wow-delay="1.6s"
+          >
+            <div className="team-wrapper">
+              <img src={team3} className="img-responsive" alt="team img" />
+              <div className="team-des">
+                <h4>Mary</h4>
+                <h3>Web Ads</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
