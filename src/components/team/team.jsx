0bceb1969a78c317934888000937f0b1f05302cf
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-23 23:54:34 +0200

    style(team.jsx): wrap the cards with cards class to style them

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -12,40 +12,42 @@ const Team = () => {
             <h1 className="heading bold background">OUR TEAM</h1>
             <h2 className="subheading">CREATIVE TALENTS FROM WEST COAST</h2>
           </div>
-          <div
-            className="col-md-4 col-sm-4 col-xs-6 wow fadeIn"
-            data-wow-delay="0.9s"
-          >
-            <div className="team-wrapper">
-              <img src={team1} className="img-responsive " alt="team img" />
-              <div className="team-des">
-                <h4>Tracy</h4>
-                <h3>Social Marketing</h3>
+          <div className="cards">
+            <div
+              className="col-md-4 col-sm-4 col-xs-12 wow fadeIn"
+              data-wow-delay="0.9s"
+            >
+              <div className="team-wrapper">
+                <img src={team1} className="img-responsive " alt="team img" />
+                <div className="team-des">
+                  <h4>Tracy</h4>
+                  <h3>Social Marketing</h3>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div
-            className="col-md-4 col-sm-4 col-xs-6 wow fadeIn"
-            data-wow-delay="1.3s"
-          >
-            <div className="team-wrapper">
-              <img src={team2} className="img-responsive" alt="team img" />
-              <div className="team-des">
-                <h4>Linda</h4>
-                <h3>Digital Marketing</h3>
+            <div
+              className="col-md-4 col-sm-4 col-xs-12 wow fadeIn"
+              data-wow-delay="1.3s"
+            >
+              <div className="team-wrapper">
+                <img src={team2} className="img-responsive" alt="team img" />
+                <div className="team-des">
+                  <h4>Linda</h4>
+                  <h3>Digital Marketing</h3>
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            className="col-md-4 col-sm-4 col-xs-6 wow fadeIn"
-            data-wow-delay="1.6s"
-          >
-            <div className="team-wrapper">
-              <img src={team3} className="img-responsive" alt="team img" />
-              <div className="team-des">
-                <h4>Mary</h4>
-                <h3>Web Ads</h3>
+            <div
+              className="col-md-4 col-sm-4 col-xs-12 wow fadeIn "
+              data-wow-delay="1.6s"
+            >
+              <div className="team-wrapper">
+                <img src={team3} className="img-responsive" alt="team img" />
+                <div className="team-des">
+                  <h4>Mary</h4>
+                  <h3>Web Ads</h3>
+                </div>
               </div>
             </div>
           </div>
