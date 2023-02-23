787a274d6d9897508c711a7a0244d92ac213c33a
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-23 13:38:01 +0200

    feat(footer.jsx): create a new component for the footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,46 @@
+import "../../index.css";
+import "./footer.css";
+import { Link } from "react-router-dom";
+
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 col-sm-12">
+            <ul className="social-icon">
+              <li>
+                <Link
+                  to="#"
+                  className="fa fa-facebook wow bounceIn"
+                  data-wow-delay="0.3s"
+                  target="_parent"
+                ></Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  className="fa fa-twitter wow bounceIn"
+                  data-wow-delay="0.6s"
+                ></Link>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  className="fa fa-behance wow bounceIn"
+                  data-wow-delay="0.9s"
+                ></Link>
+              </li>
+              
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
