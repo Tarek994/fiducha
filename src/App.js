9afa470d9d2679e7c4b266096534718734d23472
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-26 00:00:05 +0200

    feat(app.js): use the react-router-dom to navigate with all pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
-import Services from './components/services/services';
-import Portfolio from './components/portfolio/portfolio';
+import Services from "./components/services/services";
+import Portfolio from "./components/portfolio/portfolio";
 import Team from "./components/team/team";
+import Happen from "./components/happen/happen";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer.jsx";
+import CopyRight from "./components/copyright/copyright";
 
 function App() {
   return (
-    <BrowserRouter>
-        {/* <Navbar/>
-        <Home/>
-        <Services/>
-        <Portfolio/> */}
-        <Team/>
-
-      </BrowserRouter>
-    
+    <>
+      <Router>
+        <Navbar />
+        <div id="section-1"></div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Services />} />
+          <Route path="/about" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/happen" element={<Happen />} />
+        </Routes>
+        <Contact />
+        <Footer />
+        <CopyRight />
+      </Router>
+    </>
   );
 }
 
