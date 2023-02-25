4a2b1a0d0a7eea59c3f38f7c5b021398f7da923f
Tarek994 <ghali.tarek.123@hotmail.com>
2023-02-25 23:54:38 +0200

    fix(navbar-logo): remove unused style properties, add the smoothScroll class

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import "../../index.css";
 import "./navbar.css";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -12,7 +12,7 @@ const Navbar = () => {
           <span className="icon icon-bar"></span>
           <span className="icon icon-bar"></span>
         </button>
-        <Link to="home" spy={true} smooth={true} duration={900} className="navbar-brand">
+        <Link to="/" className="navbar-brand smoothScroll">
             <span className="bold logo">FIDUCHA</span>
         </Link>
 
