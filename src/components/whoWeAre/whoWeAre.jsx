c5179450d23ccc5ef3b3f7d6f50ebb5bb3e705c5
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-01 23:54:23 +0200

    style(whoWeAre.jsx): decide the columns of the component

--- src/components/whoWeAre/whoWeAre.jsx
+++ src/components/whoWeAre/whoWeAre.jsx
@@ -4,8 +4,8 @@ const whoWeAre = () => {
   return (
     <div className="main col-10 ">
       <div className="main-des col-12">
-        <div className="description"></div>
-        <div className="gallery"></div>
+        <div className="description col-lg-6 col-sm-10 col-xs-12"></div>
+        <div className="gallery col-lg-6 col-sm-10 col-xs-12"></div>
       </div>
     </div>
   );
