889f9fa5a5c71dd494509c51c201fd1b4e228f01
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-03 00:01:12 +0200

    feat(whoWeAre.jsx): add the demo lines for grid the section

--- src/components/whoWeAre/whoWeAre.jsx
+++ src/components/whoWeAre/whoWeAre.jsx
@@ -4,14 +4,22 @@ const WhoWeAre = () => {
   return (
     <div id="whoWeAre" className="main col-12 ">
       <div className="max-width col-12">
-      <div className="col-12 ">
-        <h1 className="Who-title">WHO WE ARE</h1>
-        <h2 className="subheading">what can we do for you?</h2>
-      </div>
-      <div className="main-des col-12">
-        <div className="description col-lg-6 col-sm-12 col-xs-12"> AAAAAAAA</div>
-        <div className="gallery col-lg-6 col-sm-12 col-xs-12">rrrr</div>
-      </div>
+        <div className="col-12 ">
+          <h1 className="Who-title">WHO WE ARE</h1>
+          <h2 className="subheading">what can we do for you?</h2>
+        </div>
+        <div className="main-des col-12">
+          <div className="description col-lg-6 col-sm-12 col-xs-12">
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+          </div>
+          <div className="gallery col-lg-6 col-sm-12 col-xs-12">
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+          </div>
+        </div>
       </div>
     </div>
   );
