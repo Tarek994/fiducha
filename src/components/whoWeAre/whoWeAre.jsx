2f42d648c5c1d201c5878bc80f48e34404e277a6
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-02 23:35:54 +0200

    feat(whoWeAre.jsx): add the title to the section

--- src/components/whoWeAre/whoWeAre.jsx
+++ src/components/whoWeAre/whoWeAre.jsx
@@ -1,14 +1,20 @@
 import "./whoWeAre.css";
 
-const whoWeAre = () => {
+const WhoWeAre = () => {
   return (
-    <div className="main col-10 ">
+    <div id="whoWeAre" className="main col-12 ">
+      <div className="max-width col-12">
+      <div className="col-12 ">
+        <h1 className="Who-title">WHO WE ARE</h1>
+        <h2 className="subheading">what can we do for you?</h2>
+      </div>
       <div className="main-des col-12">
-        <div className="description col-lg-6 col-sm-10 col-xs-12"></div>
-        <div className="gallery col-lg-6 col-sm-10 col-xs-12"></div>
+        <div className="description col-lg-6 col-sm-12 col-xs-12"> AAAAAAAA</div>
+        <div className="gallery col-lg-6 col-sm-12 col-xs-12">rrrr</div>
+      </div>
       </div>
     </div>
   );
 };
 
-export default whoWeAre;
+export default WhoWeAre;
