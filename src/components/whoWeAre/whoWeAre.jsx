d9a6bc91204dd7f15d615198d4f1080b90726f74
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-06 23:58:17 +0200

    feat(WhoWe.jsx): add more description to this section

--- src/components/whoWeAre/whoWeAre.jsx
+++ src/components/whoWeAre/whoWeAre.jsx
@@ -1,7 +1,20 @@
 import "./whoWeAre.css";
+import React, { useState } from "react";
+
 import InstagramPost from "./post.jsx";
 
+import hero1 from "../../images/hero1.jpg";
+import hero from "../../images/hero.jpeg";
+
+const imageUrls = [hero1, hero];
+
 const WhoWeAre = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % imageUrls.length);
+  };
+
   return (
     <div id="whoWeAre" className="main col-12 ">
       <div className="max-width col-12">
@@ -9,9 +22,12 @@ const WhoWeAre = () => {
           <h1 className="Who-title">WHO WE ARE</h1>
           <h2 className="subheading">what can we do for you?</h2>
         </div>
-        <div className="main-des col-12">
-          <div className="description col-lg-6 col-sm-12 col-xs-12">
-            <h4> What kind of people we are ? </h4>
+        <div className="main-des col-12 wow " data-wow-delay="0.9s">
+          <div
+            className="description col-lg-6 col-sm-12 col-xs-12 wow "
+            data-wow-delay="0.9s"
+          >
+            <h4> What can we do for you ? </h4>
             <br />
             <p>
               {" "}
@@ -31,8 +47,45 @@ const WhoWeAre = () => {
               dolor sit amet consectetur adipisicing
             </p>
           </div>
-          <div className="gallery col-lg-6 col-sm-12 col-xs-12">
-            <InstagramPost />
+          <div
+            className="gallery wow col-lg-6 col-sm-12 col-xs-12 wow "
+            data-wow-delay="0.9s"
+          >
+            <InstagramPost imageUrl={imageUrls[currentIndex]} />
+          </div>
+
+          <div
+            className="gallery1 wow col-lg-6 col-sm-12 col-xs-12 wow "
+            data-wow-delay="0.9s"
+          >
+            <InstagramPost
+              imageUrl={imageUrls[(currentIndex + 1) % imageUrls.length]}
+            />
+          </div>
+
+          <div
+            className="description1 col-lg-6 col-sm-12 col-xs-12 wow "
+            data-wow-delay="0.9s"
+          >
+            <h4> What can we do for you ? </h4>
+            <br />
+            <p>
+              {" "}
+              Lorem ipsum, dolor sit amet consectetur adipisicing Lorem ipsum,
+              dolor sit amet consectetur adipisicing
+            </p>
+            <br />
+            <p>
+              {" "}
+              Lorem ipsum, dolor sit amet consectetur adipisicing Lorem ipsum,
+              dolor sit amet consectetur adipisicing
+            </p>{" "}
+            <br />
+            <p>
+              {" "}
+              Lorem ipsum, dolor sit amet consectetur adipisicing Lorem ipsum,
+              dolor sit amet consectetur adipisicing
+            </p>
           </div>
         </div>
       </div>
