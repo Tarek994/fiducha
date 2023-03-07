95fe2a0026ab6f8ed50503aba2b8984be3279301
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-07 23:28:04 +0200

    feat(whoWeAre.jsx): change the instagramPost with a images

--- src/components/whoWeAre/whoWeAre.jsx
+++ src/components/whoWeAre/whoWeAre.jsx
@@ -1,20 +1,10 @@
 import "./whoWeAre.css";
-import React, { useState } from "react";
-
-import InstagramPost from "./post.jsx";
+import React from "react";
 
 import hero1 from "../../images/hero1.jpg";
 import hero from "../../images/hero.jpeg";
 
-const imageUrls = [hero1, hero];
-
 const WhoWeAre = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleNextClick = () => {
-    setCurrentIndex((currentIndex + 1) % imageUrls.length);
-  };
-
   return (
     <div id="whoWeAre" className="main col-12 ">
       <div className="max-width col-12">
@@ -22,7 +12,7 @@ const WhoWeAre = () => {
           <h1 className="Who-title">WHO WE ARE</h1>
           <h2 className="subheading">what can we do for you?</h2>
         </div>
-        <div className="main-des col-12 wow " data-wow-delay="0.9s">
+        <div className="main-des col-lg-12 wow " data-wow-delay="0.9s">
           <div
             className="description col-lg-6 col-sm-12 col-xs-12 wow "
             data-wow-delay="0.9s"
@@ -47,24 +37,19 @@ const WhoWeAre = () => {
               dolor sit amet consectetur adipisicing
             </p>
           </div>
-          <div
-            className="gallery wow col-lg-6 col-sm-12 col-xs-12 wow "
-            data-wow-delay="0.9s"
-          >
-            <InstagramPost imageUrl={imageUrls[currentIndex]} />
+          <div className="gallery wow col-lg-6 col-sm-12 col-xs-12  ">
+            <img className="who-image" src={hero} alt="" />
           </div>
+        </div>
 
-          <div
-            className="gallery1 wow col-lg-6 col-sm-12 col-xs-12 wow "
-            data-wow-delay="0.9s"
-          >
-            <InstagramPost
-              imageUrl={imageUrls[(currentIndex + 1) % imageUrls.length]}
-            />
+        <div className="line"></div>
+
+        <div className="main-des second col-lg-12 wow " data-wow-delay="0.9s">
+          <div className="gallery wow col-lg-6 col-sm-12 col-xs-12  ">
+            <img className="who-image" src={hero1} alt="" />
           </div>
-
           <div
-            className="description1 col-lg-6 col-sm-12 col-xs-12 wow "
+            className="description col-lg-6 col-sm-12 col-xs-12 wow "
             data-wow-delay="0.9s"
           >
             <h4> What can we do for you ? </h4>
