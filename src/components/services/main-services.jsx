9c23d49ab80c89075c2bf330c9df6dda9a8cc635
Tarek994 <ghali.tarek.123@hotmail.com>
2023-03-10 23:42:15 +0200

    fix(mainServices.jsx): fix the bug of centering the cards in the middle

--- src/components/services/main-services.jsx
+++ src/components/services/main-services.jsx
@@ -1,83 +1,64 @@
 import Card from "react-bootstrap/Card";
 import download from "./images/download.jpeg";
 import { Link } from "react-router-dom";
-import "./services.css";
+import "./new.css";
 
 const MainServices = () => {
   return (
     <div id="services" className="main-services">
-      <div className="col-md-12 col-sm-12 text-center">
-        <h1 className="heading bold title">SERVICES</h1>
-        <h2 className="subheading">Strategic planning. Measurable results</h2>
+      <div className="row text-center">
+        <div className="col-md-12 col-sm-12">
+          <h1 className="heading bold title">SERVICES</h1>
+          <h2 className="subheading">Strategic planning. Measurable results</h2>
+        </div>
       </div>
 
-      <div className="row services-cards">
-        <div className="row">
-          <div className="d-flex justify-content-center align-items-center">
-            <div className="col-md-6 col-sm-12 d-flex justify-content-center mb-4">
-              <Card
-                style={{
-                  width: "60%",
-                  margin: "0 auto",
-                  paddingBottom: "10px",
-                  marginRight: "4rem",
-                  boxShadow: "0 5px 25px rgba(1 1 1 /15%)",
-                }}
-              >
-                <div style={{ position: "relative" }}>
-                  <Card.Img
-                    variant="top"
-                    src={download}
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </div>
-                <Card.Body className="body-center">
-                  <Card.Title className="bold">Digital marketing</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Link to="/digitalMarketing" className="btn btn-primary">
-                    Go to Services
-                  </Link>
-                </Card.Body>
-              </Card>
-            </div>
+      <div className="row services-cards justify-content-center">
+        <div className="col-md-6 col-sm-12 mb-4">
+          <div className="cardss-1">
+            <Card className="card-style">
+              <div style={{ position: "relative" }}>
+                <Card.Img
+                  variant="top"
+                  src={download}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <Card.Body className="body-center">
+                <Card.Title className="bold">Digital marketing</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Link to="/digitalMarketing" className="btn btn-primary">
+                  Go to Services
+                </Link>
+              </Card.Body>
+            </Card>
           </div>
+        </div>
 
-          <div
-            className="d-flex justify-content-center align-items-center"
-            style={{ height: "100vh" }}
-          >
-            <div className="col-md-6 col-sm-12 d-flex justify-content-center mb-4">
-              <Card
-                style={{
-                  width: "60%",
-                  margin: "0 auto",
-                  paddingBottom: "10px",
-                  marginLeft: "4rem",
-                  boxShadow: "0 5px 25px rgba(1 1 1 /15%)",
-                }}
-              >
-                <div style={{ position: "relative" }}>
-                  <Card.Img
-                    variant="top"
-                    src={download}
-                    style={{ width: "100%", height: "auto" }}
-                  />
-                </div>
-                <Card.Body className="body-center">
-                  <Card.Title className="bold"> MARKETING</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Link to="/marketing" className="btn btn-primary">
-                    Go to Services
-                  </Link>
-                </Card.Body>
-              </Card>
-            </div>
+        <div className="col-md-6 col-sm-12 mb-4">
+          <div className="cardss-2">
+            <Card className="card-style">
+              <div style={{ position: "relative" }}>
+                <Card.Img
+                  variant="top"
+                  src={download}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <Card.Body className="body-center">
+                <Card.Title className="bold">MARKETING</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Link to="/marketing" className="btn btn-primary">
+                  Go to Services
+                </Link>
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </div>
