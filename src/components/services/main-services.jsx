4faac031dc9e4c4397e0dfdb7e546c7bf87fbcd6
Tarek994 <ghali.tarek.123@hotmail.com>
2023-05-06 04:45:39 +0300

    feat(main-services-cards): swapping the cards with adding the provided services

--- src/components/services/main-services.jsx
+++ src/components/services/main-services.jsx
@@ -10,12 +10,14 @@ const MainServices = () => {
       <div className="row text-center">
         <div className="col-md-12 col-sm-12">
           <h1 className="heading bold title">SERVICES</h1>
-          <h2 className="subheading bold">Strategic planning. Measurable results</h2>
+          <h2 className="subheading bold">
+            Strategic planning. Measurable results
+          </h2>
         </div>
       </div>
 
       <div className="row services-cards ">
-        <div className="col-md-6 col-sm-12 mb-4 ">
+        <div className="main-cards ">
           <div className="cardss-1">
             <Card className="card-style">
               <div style={{ position: "relative" }}>
@@ -26,21 +28,26 @@ const MainServices = () => {
                 />
               </div>
               <Card.Body className="body-center">
-                <Card.Title className="bold">Digital marketing</Card.Title>
+                <Card.Title className="bold ">ATL</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  <p className="offers">
+                    Signboard
+                    <br />
+                    Outdoor
+                    <br />
+                    Outdoor Booking
+                    <br />
+                    TV
+                  </p>
                 </Card.Text>
-                <Link to="/fiducha/services/digitalMarketing" className="btn btn-primary">
-                  Go to Services
+                <Link to="" className="btn btn-primary-ser">
+                  Get it now
                 </Link>
               </Card.Body>
             </Card>
           </div>
-        </div>
 
-        {/* <div className="col-md-6 col-sm-12 mb-4">
-          <div className="cardss-2">
+          <div className="cardss-1">
             <Card className="card-style">
               <div style={{ position: "relative" }}>
                 <Card.Img
@@ -50,13 +57,74 @@ const MainServices = () => {
                 />
               </div>
               <Card.Body className="body-center">
-                <Card.Title className="bold">MARKETING</Card.Title>
+                <Card.Title className="bold">BTL</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  <p className="offers">
+                    Booths
+                    <br />
+                    Indoor
+                    <br />
+                    Shop Branding
+                    <br />
+                    Event Setup
+                    <br />
+                    Planning
+                    <br />
+                    Event Management
+                    <br />
+                    Catering
+                    <br />
+                    3D Simulation
+                    <br />
+                    Production Rentals
+                    <br />
+                    Events Production
+                    <br />
+                    Rentals
+                    <br />
+                    Printing
+                  </p>
                 </Card.Text>
-                <Link to="/fiducha/services/marketing" className="btn btn-primary">
-                  Go to Services
+                <Link to="" className="btn btn-primary-ser">
+                  Get it now
+                </Link>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="cardss-1">
+            <Card className="card-style">
+              <div style={{ position: "relative" }}>
+                <Card.Img
+                  variant="top"
+                  src={download}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <Card.Body className="body-center">
+                <Card.Title className="bold">TTL</Card.Title>
+                <Card.Text>
+                  <p className="offers">
+                    photography
+                    <br />
+                    Motion Graphics
+                    <br />
+                    Video Production
+                    <br />
+                    Social Media
+                    <br />
+                    SEO
+                    <br />
+                    Web Development
+                    <br />
+                    Brand Identity
+                    <br />
+                    Google Services
+                    <br />
+                  </p>
+                </Card.Text>
+                <Link to="" className="btn btn-primary-ser">
+                  Get it now
                 </Link>
               </Card.Body>
             </Card>
